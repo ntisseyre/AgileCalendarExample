3ab3eb1bbd22e7515f53a0e85637c672aa512ca1
supperslonic <natalia.a.zelenskaya@gmail.com>
2014-01-07 14:26:18 -0600

    changed layout of release Cycle editing control

--- AgileCalendarExample/Content/js/agileReleaseCycle.js
+++ AgileCalendarExample/Content/js/agileReleaseCycle.js
@@ -3,5 +3,5 @@
     var agileReleaseCycle = $('.agile-releaseCycle');
 
     //bind ColorPicker
-    agileReleaseCycle.find('> div.agile-item-colored > div:last-child').each(function () { initColorPicker($(this)); });
+    agileReleaseCycle.find(' > div > div.agile-item-colored > div:last-child').each(function () { initColorPicker($(this)); });
 });
